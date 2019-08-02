1009f07cd27d66a639451f83093cfe90c8f3e360
Alice Brunel <alc.brunel@gmail.com>
2019-08-02 22:58:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if sender.valid? && self.status != 'complete'
+    if valid? = true && self.status != 'complete'
       receiver.balance = receiver.balance + @amount
       sender.balance = sender.balance - @amount
       @status = 'complete'
