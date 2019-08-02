375cede7a80b2b4856c62cf8a9b207f4789cd40f
Alice Brunel <alc.brunel@gmail.com>
2019-08-02 22:48:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :transfer_amount
+  
+  
+  def initialize(sender, receiver, transfer_amount)
+   @sender = sender
+   @receiver = receiver
+   @status = 'pending'
+   @transfer_amount = transfer_amount
+  end
+  
+  def valid?
+      
+  end
+  
 end
